01112f51bd0a66f5d241583af21d971bb5203b48
ThangNguyen312v <tatthang112113@gmail.com>
2024-08-19 18:12:47 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace songuyen{
+    class Program{
+        static void Main(string[] Args){
+            int nums = 0;
+            int N = 2;
+            int count = 0;
+            Console.WriteLine("nhap so");
+            nums = Int16.Parse(Console.ReadLine());
+            
+            
+            
+            while(count<nums){
+                
+                
+                int m = (int)Math.Sqrt(N);
+                for(int i = 2; i <= m;i ++){
+                    if(N % i == 0){
+                        Console.WriteLine(N);
+                        count++;
+                        break;
+
+                    }
+                } N++;
+            }
+           
+            }
+
+
+        }
+    }
+
